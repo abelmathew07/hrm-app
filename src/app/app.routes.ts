ec7393b567ed7c1701a73d197376589ba09bf40a
amal-stacqube <amal@stacqube.com>
2024-09-05 11:52:56 +0530

    added router links and created department component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
+import { DepartmentComponent } from './department/department.component';
 
 export const routes: Routes = [
   { path: '', component: DashboardComponent },
-  { path: 'employees', component: EmployeeListComponent },
+  { path: 'employees-list', component: EmployeeListComponent },
   { path: 'employees/:id', component: EmployeeDetailComponent },
+  {path:'department', component:DepartmentComponent}
 ];
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-header',
+  imports:[RouterLink,RouterOutlet],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
