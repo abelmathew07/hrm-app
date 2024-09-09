064c7d944425443ab63ea493b33af5308f020e01
amal-stacqube <amal@stacqube.com>
2024-09-09 15:49:16 +0530

    add few components, alligned all

---
+++ src/app/dashboard/dashboard-clients/dashboard-clients.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-clients',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-clients.component.html',
+  styleUrl: './dashboard-clients.component.css'
+})
+export class DashboardClientsComponent {
+
+}
