31c5b58bc44a4819685f2a1317aa083ae03562c7
amal-stacqube <amal@stacqube.com>
2024-09-10 13:06:50 +0530

    added new table payment, made necessory changes

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -4,13 +4,15 @@ import { CardsComponent } from "./cards/cards.component";
 import { DashboardTasksComponent } from "./dashboard-tasks/dashboard-tasks.component";
 import { DashboardProjectsComponent } from "./dashboard-projects/dashboard-projects.component";
 import { DashboardClientsComponent } from "./dashboard-clients/dashboard-clients.component";
+import { DashboardPaymentsComponent } from "./dashboard-payments/dashboard-payments.component";
 
 @Component({
   standalone: true,
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
-  imports: [NgFor, CardsComponent, DashboardTasksComponent, DashboardProjectsComponent, DashboardClientsComponent] // or CommonModule
+  imports: [NgFor, CardsComponent, DashboardTasksComponent, DashboardProjectsComponent, DashboardClientsComponent, DashboardPaymentsComponent] // or CommonModule
+ // or CommonModule
  // or CommonModule
  // or CommonModule
  // or CommonModule
