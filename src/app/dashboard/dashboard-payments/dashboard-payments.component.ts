692b153288ebf8333972875701c0a125fda99fde
amal-stacqube <amal@stacqube.com>
2024-09-12 15:17:36 +0530

    made tables in dashboard dynamic using a dummy data

--- src/app/dashboard/dashboard-payments/dashboard-payments.component.ts
+++ src/app/dashboard/dashboard-payments/dashboard-payments.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { paymentdata } from '../dummydata';
+import { CommonModule, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard-payments',
   standalone: true,
-  imports: [],
+  imports: [NgFor,CommonModule],
   templateUrl: './dashboard-payments.component.html',
   styleUrl: './dashboard-payments.component.css'
 })
 export class DashboardPaymentsComponent {
-
+  payments = paymentdata;
 }
---
+++ src/app/dashboard/dummydata.ts
@@ -0,0 +1,184 @@
+export const clientdata = [
+    {
+        clientName: 'Client 1',
+        projectName: 'Project 1',
+        phone: '(123) 456-7890',
+        email: 'client1example.com',
+        id: 1,
+        img: '/clientImg/image1.png'
+    },
+    {
+        clientName: 'Client 2', 
+        projectName: 'Project 2',
+        phone: '(123) 456-7890',
+        email: 'client2example.com',
+        id: 2,
+        img: '/clientImg/image1.png'
+    },
+    {
+        clientName: 'Client 3',
+        projectName: 'Project 3',
+        phone: '(123) 456-7890',   
+        email: 'client3example.com',
+        id: 3,
+        img: '/clientImg/image1.png'
+    },
+    {
+        clientName: 'Client 4',
+        projectName: 'Project 4',
+        phone: '(123) 456-7890',
+        email: 'client1example.com',
+        id: 4,
+        img: '/clientImg/image1.png'
+    },
+    {
+        clientName: 'Client 5',
+        projectName: 'Project 5',
+        phone: '(123) 456-7890',
+        email: 'client1example.com',
+        id: 5,
+        img: '/clientImg/image1.png'
+    },
+    {
+        clientName: 'Client 6', 
+        projectName: 'Project 6',
+        phone: '(123) 456-7890',
+        email: 'client2example.com',
+        id: 6,
+        img: '/clientImg/image1.png'
+    },
+    {
+        clientName: 'Client 7',
+        projectName: 'Project 7',
+        phone: '(123) 456-7890',   
+        email: 'client3example.com',
+        id: 7,
+        img: '/clientImg/image1.png'
+    },
+    {
+        clientName: 'Client 8',
+        projectName: 'Project 8',
+        phone: '(123) 456-7890',
+        email: 'client1example.com',
+        id: 8,
+        img: '/clientImg/image1.png'
+    },
+    {
+        clientName: 'Client 9',
+        projectName: 'Project 9',
+        phone: '(123) 456-7890',
+        email: 'client1example.com',
+        id: 9,
+        img: '/clientImg/image1.png'
+    }
+]
+
+export const projectdata = [
+    {
+        projectName: 'Project 1',
+        status: 'Active',
+        id: 1,
+        deadline: '2022-01-01',
+    },
+    {
+        projectName: 'Project 2',
+        status: 'Active',
+        id: 2,
+        deadline: '2022-01-01',
+    },
+    {
+        projectName: 'Project 3',
+        status: 'Active',
+        id: 3,
+        deadline: '2022-01-01',
+    },
+    {
+        projectName: 'Project 4',
+        status: 'Active',
+        id: 4,
+        deadline: '2022-01-01',
+    },
+    {
+        projectName: 'Project 5',
+        status: 'Active',
+        id: 5,
+        deadline: '2022-01-01',
+    },
+    {
+        projectName: 'Project 6',
+        status: 'Active',
+        id: 6,
+        deadline: '2022-01-01',
+    },
+    {
+        projectName: 'Project 7',
+        status: 'Active',
+        id: 7,
+        deadline: '2022-01-01',
+    },
+    {
+        projectName: 'Project 8',
+        status: 'Active',
+        id: 8,
+        deadline: '2022-01-01',
+    }
+]
+
+export const paymentdata = [
+    {
+        id: 1,
+        invoiceId: 1,
+        clientName: 'Client 1',
+        paymentDate: '2022-01-01',
+        paidAmount: 1000
+    },
+    {
+        id: 2,
+        invoiceId: 2,
+        clientName: 'Client 2',
+        paymentDate: '2022-01-01',
+        paidAmount: 2000
+    },
+    {
+        id: 3,
+        invoiceId: 3,
+        clientName: 'Client 3',
+        paymentDate: '2022-01-01',
+        paidAmount: 3000
+    },
+    {
+        id: 4,
+        invoiceId: 4,
+        clientName: 'Client 4', 
+        paymentDate: '2022-01-01',
+        paidAmount: 4000
+    },
+    {
+        id: 5,
+        invoiceId: 5,
+        clientName: 'Client 5',
+        paymentDate: '2022-01-01',
+        paidAmount: 1000
+    },
+    {
+        id: 6,
+        invoiceId: 6,
+        clientName: 'Client 6',
+        paymentDate: '2022-01-01',
+        paidAmount: 2000
+    },
+    {
+        id: 7,
+        invoiceId: 7,
+        clientName: 'Client 7',
+        paymentDate: '2022-01-01',
+        paidAmount: 3000
+    },
+    {
+        id: 8,
+        invoiceId: 8,
+        clientName: 'Client 8', 
+        paymentDate: '2022-01-01',
+        paidAmount: 4000
+    }
+]
