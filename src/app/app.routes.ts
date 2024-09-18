a7464d7d8dd63395cdd14bda8d137ea88e3fe61b
amal-stacqube <amal@stacqube.com>
2024-09-18 13:08:26 +0530

    added project page route and added in sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,13 @@ import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { DepartmentComponent } from './department/department.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { ClientListComponent } from './client-list/client-list.component';
+import { ProjectComponent } from './project/project.component';
 
 export const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'employees-list', component: EmployeeListComponent },
   {path:'department', component:DepartmentComponent},
   {path:'login-page', component:LoginPageComponent},
-  {path:'client-list', component:ClientListComponent}
+  {path:'client-list', component:ClientListComponent},
+  {path:'project',component:ProjectComponent}
 ];
